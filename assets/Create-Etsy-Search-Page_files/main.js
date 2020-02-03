a189adfd0665a67a8c8b9a1681a5bb7ebeac91d7
Robert Bishop <rbishop2006@gmail.com>
2020-02-02 17:44:37 -0800

    tenth commit

---
+++ assets/Create-Etsy-Search-Page_files/main.js
@@ -0,0 +1,22 @@
+let html = items.results
+  .map(function(item) {
+    var eur = ""
+    if (item.currency_code === "GBP") {
+      eur += "&pound;"
+    } else if (item.currency_code === "USD") {
+      eur += "$"
+    } else if (item.currency_code === "EUR") {
+      eur += "&euro;"
+    }
+    return `
+        <a href="${item.url}"><div class="productDisplay">
+        <img src="${item.Images[0].url_170x135}"  />
+        <div class="desricption"></div>
+        <p class="title">${item.title}</p>
+        <p class="shop_name">${item.Shop.shop_name}</p>
+        <p class="price">${eur}${item.price}</p>
+      </div></a>`
+  })
+  .join("")
+
+document.querySelector(".products").innerHTML = html
